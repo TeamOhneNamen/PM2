7924c82908f372307e5af0fef70f623940450563
th0rben <opi70@web.de>
2017-03-21 14:21:28 +0100

    a

---
+++ src/de/hawhamburg/ton/P01/Complex.java
@@ -0,0 +1,35 @@
+package de.hawhamburg.ton.P01;
+
+public class Complex implements {
+
+	private int erste;
+	private int zweite;
+	
+	public Complex() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	public int getErste() {
+		return erste;
+	}
+
+
+
+	public int getZweite() {
+		return zweite;
+	}
+
+
+
+	public static void main(String erste, String zweite) {
+		
+	}
+	
+	public String toString(){
+		
+		return("("+erste+" , "+zweite+")");
+	}
+	
+}
