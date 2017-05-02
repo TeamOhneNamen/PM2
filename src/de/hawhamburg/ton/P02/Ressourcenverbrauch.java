b545e813e0ba0cd24613eae801a1f14016ff52c5
Gargamel19 <trendelenburger19.04@googlemail.com>
2017-05-02 16:32:58 +0200

    hj

--- src/de/hawhamburg/ton/P02/Ressourcenverbrauch.java
+++ src/de/hawhamburg/ton/P02/Ressourcenverbrauch.java
@@ -1,5 +1,7 @@
 package de.hawhamburg.ton.P02;
 
+import java.util.Date;
+
 /**
  * @author Thorben Schomacker
  * @author Ferdinand Trendelenburg
@@ -14,8 +16,8 @@ public class Ressourcenverbrauch {
 	public static void compare(){
 		int places = 5;
 		
-		final int oneDec = 1;
-		final int twoDec = 2;
+		final int oneDec = 53;
+		final int twoDec = 2724;
 		int resultDec;
 		
 		final byte oneBin = 00000001;
@@ -29,72 +31,77 @@ public class Ressourcenverbrauch {
 		long afterBin;
 		
 		// Addition compared in milliseconds.
-		beforeDec = System.currentTimeMillis();
+		beforeDec = new Date().getTime();
 		resultDec = oneDec + twoDec;
-		afterDec = System.currentTimeMillis();
-		System.out.println("Addition compared in milliseconds(decimal): " + "\t\t" + Long.toString((afterDec - beforeDec)));
+		resultDec = twoDec + twoDec;
+		resultDec = twoDec + oneDec;
+		resultDec = oneDec + oneDec;
+		resultDec = oneDec + twoDec;
+		resultDec = oneDec + twoDec;
+		afterDec = new Date().getTime();
+		System.out.println("Addition compared in milliseconds(decimal): " + "\t\t" + (afterDec - beforeDec) + "\t" + "=" + afterDec + "-" + beforeDec);
 		
 		beforeBin = System.currentTimeMillis();
 		resultBin = oneBin + twoBin;
 		afterBin = System.currentTimeMillis();
-		System.out.println("Addition compared in milliseconds(binary): " + "\t\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Addition compared in milliseconds(binary): " + "\t\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		// Addition compared in nanoseconds.
 		beforeDec = System.nanoTime();
 		resultDec = oneDec + twoDec;
 		afterDec = System.nanoTime();
-		System.out.println("Addition compared in nanoseconds(decimal): " + "\t\t" + Long.toString(afterDec - beforeDec));
+		System.out.println("Addition compared in nanoseconds(decimal): " + "\t\t" + (afterDec - beforeDec) + "\t" + "=" + afterDec + "-" + beforeDec);
 		
 		beforeBin = System.nanoTime();
 		resultBin = oneBin + twoBin;
 		afterBin = System.nanoTime();
-		System.out.println("Addition compared in nanoseconds(binary): " + "\t\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Addition compared in nanoseconds(binary): " + "\t\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		// Multiplication compared in milliseconds.
 		beforeDec = System.currentTimeMillis();
 		resultDec = oneDec * twoDec;
 		afterDec = System.currentTimeMillis();
-		System.out.println("Multiplication compared in milliseconds(decimal): " + "\t" + Long.toString(afterDec - beforeDec));
+		System.out.println("Multiplication compared in milliseconds(decimal): " + "\t" + (afterDec - beforeDec) + "\t" + "=" + afterDec + "-" + beforeDec);
 		
 		beforeBin = System.currentTimeMillis();
 		resultBin = oneBin * twoBin;
 		afterBin = System.currentTimeMillis();
-		System.out.println("Multiplication compared in milliseconds(binary): " + "\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Multiplication compared in milliseconds(binary): " + "\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		// Multiplication compared in nanoseconds.
 		beforeDec = System.nanoTime();
 		resultDec = oneDec * twoDec;
 		afterDec = System.nanoTime();
-		System.out.println("Multiplication compared in nanoseconds(decimal): " + "\t" + Long.toString(afterDec - beforeDec));
+		System.out.println("Multiplication compared in nanoseconds(decimal): " + "\t" + (afterDec - beforeDec) + "\t" + "=" + afterDec + "-" + beforeDec);
 		
 		beforeBin = System.nanoTime();
 		resultBin = oneBin * twoBin;
 		afterBin = System.nanoTime();
-		System.out.println("Multiplication compared in nanoseconds(binary): " + "\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Multiplication compared in nanoseconds(binary): " + "\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		// Left shift / right shift  in milliseconds.
 		
 		beforeBin = System.currentTimeMillis();
 		resultBin = oneBin << 5;
 		afterBin = System.currentTimeMillis();
-		System.out.println("Left shift / right shift  in milliseconds(decimal): " + "\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Left shift / right shift  in milliseconds(decimal): " + "\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		beforeBin = System.currentTimeMillis();
 		resultBin = oneBin >> 5;
 		afterBin = System.currentTimeMillis();
-		System.out.println("Left shift / right shift  in milliseconds(binary): " + "\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Left shift / right shift  in milliseconds(binary): " + "\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		// Left shift / right shift in nanoseconds.
 		
 		beforeBin = System.nanoTime();
 		resultBin = oneBin << 5;
 		afterBin = System.nanoTime();
-		System.out.println("Left shift / right shift  in nanoseconds(decimal): " + "\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Left shift / right shift  in nanoseconds(decimal): " + "\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		beforeBin = System.nanoTime();
 		resultBin = oneBin >> 5;
 		afterBin = System.nanoTime();
-		System.out.println("Left shift / right shift  in nanoseconds(binary): " + "\t" + Long.toString(afterBin - beforeBin));
+		System.out.println("Left shift / right shift  in nanoseconds(binary): " + "\t" + (afterBin - beforeBin) + "\t" + "=" + afterBin + "-" + beforeBin);
 		
 		//System.out.println(Long.toString(System.currentTimeMillis()));
 		//System.out.println(Integer.toBinaryString(2)); 
